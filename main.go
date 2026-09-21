26e5acfb6b5aa9444a3a61b6d71010388541c029
Elizabethppppp <pirogelizaveta237@gmail.com>
2026-09-21 11:55:58 +0300

    start link_watcher project

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	if err := http.ListenAndServe(":8090", mux); err != nil {
+		log.Fatal(err)
+	}
+
+}
